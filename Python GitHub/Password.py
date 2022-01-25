65c384ec6506c9286ed062e96ca88f5118eabd37
bhaveshgarg2000 <bhaveshgarg2005@gmail.com>
2022-01-26 00:59:20 +0530

    Course-Era assessment Practice

---
+++ Python GitHub/Password.py
@@ -0,0 +1,7 @@
+alphabet = 26
+
+letters = int(input("Enter the number of letters should be in the password : "));
+
+no_of_possibilities = alphabet**letters
+
+print("No.of possible passwords formed with 6 letters are : ",no_of_possibilities)
